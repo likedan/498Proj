9858f157dee1efe52bf24a012dbf112be2b66674
Kedan Li <likedan5@icloud.com>
2016-12-06 18:57:44 -0600

    out put result

--- source/kmeans.py
+++ source/kmeans.py
@@ -3,55 +3,12 @@ import random
 import sys
 
 class KMeans(object):
-    def __init__(self, persons=None, similarity_calculator=None):
-        self._similarity_calculator = similarity_calculator
-        self._persons = persons
-    
-    def _assignToClusters(self, data_points, centroids):
-        clusters = defaultdict(list)
-        
-        #for all data points(friends in our case), findout which centroid they are most similar to
-        #assign them to most similiar centroid
-        for data_point in data_points:
-            maximum_similarity = 0
-            maximum_similarity_centroid = None
-            for centroid in centroids:
-                similarity = self._similarity_calculator(self._persons, data_point, centroid)
-                if similarity > maximum_similarity:
-                    maximum_similarity = similarity
-                    maximum_similarity_centroid = centroid
-            if maximum_similarity_centroid:
-                clusters[maximum_similarity_centroid].append(data_point)
-        
-        #return the computed clusters
-        return clusters
-                
-    def _trim_clusters(self, clusters, similarity_diff_threshold):
-        for centroid in clusters:
-            #compute maximum and minimum similarity from centroid for each circle/cluster
-            minimum_similarity = sys.maxint
-            maximum_similarity = -sys.maxint
+    def __init__(self, people=None, similarity_calculator=None):
+        self.similarity_calculator = similarity_calculator
+        self.people = people
 
-            for friend in clusters[centroid]:
-                similarity = self._similarity_calculator(self._persons, centroid, friend)
-                if similarity < minimum_similarity:
-                    minimum_similarity = similarity
-                if similarity > maximum_similarity:
-                    maximum_similarity = similarity
-            similarity_difference_in_cluster = maximum_similarity - minimum_similarity
-
-            if similarity_difference_in_cluster > similarity_diff_threshold:
-                friends_to_remove = []
-                for friend in clusters[centroid]:
-                    similarity = self._similarity_calculator(self._persons, centroid, friend)
-                    if similarity < maximum_similarity - similarity_diff_threshold:
-                        friends_to_remove.append(friend)
-                for friend_to_remove in friends_to_remove:
-                    clusters[centroid].remove(friend_to_remove)
-     
-        return clusters
-        
-    def computeClusters(self, data_points, k, similarity_diff_threshold):
+    #compute the kmeans
+    def fit(self, data_points, k, similarity_diff_threshold):
         data_points = list(data_points)
         centroids = []
         # choose random k points as seed
@@ -62,8 +19,51 @@ class KMeans(object):
             centroids.append(data_points[i])
 
         clusters = self._assignToClusters(data_points, centroids)
-        
+
         return self._trim_clusters(clusters, similarity_diff_threshold)
+
+    #assign each data point to the most similiar centroid
+    def _assignToClusters(self, data_points, centroids):
+        clusters = defaultdict(list)
+        
+        for point in data_points:
+            maximum_similarity = 0
+            maximum_similarity_centroid = None
+            for centroid in centroids:
+                similarity = self.similarity_calculator(self.people, point, centroid)
+                if similarity > maximum_similarity:
+                    maximum_similarity = similarity
+                    maximum_similarity_centroid = centroid
+            if maximum_similarity_centroid:
+                clusters[maximum_similarity_centroid].append(point)
+        
+        #return the computed clusters
+        return clusters
+
+    # compute maximum and minimum similarity from centroid for each ccluster
+    def _trim_clusters(self, clusters, similarity_diff_threshold):
+        for centroid in clusters:
+            min_sim = sys.maxint
+            max_sim = -sys.maxint
+
+            for friend in clusters[centroid]:
+                similarity = self.similarity_calculator(self.people, centroid, friend)
+                if similarity < min_sim:
+                    min_sim = similarity
+                if similarity > max_sim:
+                    max_sim = similarity
+            similarity_difference_in_cluster = max_sim - min_sim
+
+            if similarity_difference_in_cluster > similarity_diff_threshold:
+                friends_to_remove = []
+                for friend in clusters[centroid]:
+                    similarity = self.similarity_calculator(self.people, centroid, friend)
+                    if similarity < max_sim - similarity_diff_threshold:
+                        friends_to_remove.append(friend)
+                for friend_to_remove in friends_to_remove:
+                    clusters[centroid].remove(friend_to_remove)
+     
+        return clusters
         
     
     
--- source/run.py
+++ source/run.py
@@ -3,33 +3,7 @@ from evaluation import *
 from dataStructure import *
 from math import sqrt
 import similarityCalculator
-
-# def writeSubmission(filename, circleMap, test=False):
-#     f = open(filename, 'w+')
-#
-#     f.write('UserId,Predicted\n')
-#
-#     for person, circles in circleMap.iteritems():
-#
-#         line = person + ','
-#
-#         if not test:
-#             for i in range(len(circles)):#circle in circles:
-#                 for j in range(len(circles[i])):#friend in circles[i]:
-#                     line += circles[i][j]
-#                     if j != len(circles[i]) - 1:
-#                         line += ' '
-#                 if i != len(circles) - 1:
-#                     line += ';'
-#         else:
-#             for friend in circles:
-#                 line += friend + ' '
-#             line += ';'
-#
-#         line += '\n'
-#         f.write(line)
-#
-#     f.close()
+import json
 
 def convert_to_output_format(clusters):
     clusters_formatted = {}
@@ -57,46 +31,28 @@ def evaluate(result):
 
 if __name__ == '__main__':
 
-    print 'Loading input data.'
     data = DataStructure()
 
-    # List of people to calculate training data for.
-    trainingPeople = []
-    for key in data.trainingDict:
-        trainingPeople.append(key)
-
     SimilarityCalc = similarityCalculator.SimilarityCalculator(len(data.featureList))
-
     training_clusters = {}
-    k_means = KMeans(data.people, SimilarityCalc.similarity_weighted_attributes_friendship)
+    kmean = KMeans(data.people, SimilarityCalc.similarity_weighted_attributes_friendship)
 
     for personID in data.originalPeople:
         friends_of_person = data.people.getPerson(personID).friends
         #approximate number of centers
         k = int(sqrt(len(friends_of_person))) + 1
-        clusters = k_means.computeClusters(friends_of_person, k, 3.5)
+        clusters = kmean.fit(friends_of_person, k, 3.5)
         training_clusters[personID] = clusters
 
     training_clusters = convert_to_output_format(training_clusters)
 
-    evaluate({k:training_clusters[k] for k in data.trainingDict})
+    result_for_trainingset = {k:training_clusters[k] for k in data.trainingDict}
+    evaluate(result_for_trainingset)
 
-    testingPeople = [origPerson for origPerson in data.originalPeople if origPerson
-            not in trainingPeople]
-    # writeSubmission(real_training_data, data.trainingMap)
-    #
-    #
-    # # Validation tests
-    # writeSubmission(kmeans_attrs, {k:attribute_clusters[k] for k in data.trainingMap})
-    # writeSubmission(kmeans_attrs_friends, {k:attribute_and_friendship_clusters[k] for k in data.trainingMap})
-    # writeSubmission(kmeans_weighted_attrs_friends, {k:weighted_attribute_and_friendship_clusters[k] for k in data.trainingMap})
-    # # Kaggle submissions
-    # writeSubmission(kmeans_kaggle_attrs, {k:attribute_clusters[k] for k in
-    #     data.originalPeople if k not in data.trainingMap})
-    # writeSubmission(kmeans_kaggle_attrs_friends,
-    #         {k:attribute_and_friendship_clusters[k] for k in
-    #             data.originalPeople if k not in data.trainingMap})
-    #
-    # writeSubmission(kmeans_kaggle_weighted_attrs_friends,
-    #         {k:weighted_attribute_and_friendship_clusters[k] for k in
-    #             data.originalPeople if k not in data.trainingMap})
+    with open('trainingset_result.json', 'w+') as outfile:
+        json.dump(result_for_trainingset, outfile)
+    #evaluate the testing result
+    result_for_testing = {k:training_clusters[k] for k in data.originalPeople if k not in data.trainingDict}
+    # evaluate(result_for_trainingset)
+    # with open('testingset_result.json', 'w+') as outfile:
+    #     json.dump(result_for_testing, outfile)
